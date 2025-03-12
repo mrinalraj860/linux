f21cbe26598a23cdf929acc72b0480744ece3fd9
Mrinal Raj <mrinalraj@Mrinals-MacBook-Pro-401.local>
2025-03-12 02:34:33 +0000

    PTE added ; pgtable

--- arch/x86/include/asm/pgtable.h
+++ arch/x86/include/asm/pgtable.h
@@ -67,7 +67,9 @@ extern pmdval_t early_pmd_flags;
 #ifdef CONFIG_PARAVIRT_XXL
 #include <asm/paravirt.h>
 #else /* !CONFIG_PARAVIRT_XXL */
-#define set_pte(ptep, pte)		struct task_struct *p = current; 		p->pg_stats.pte_set_count++		native_set_pte(ptep, pte)
+#define set_pte(ptep, pte)      \
+	struct task_struct *p = \
+		current p->pg_stats.pte_set_count++ native_set_pte(ptep, pte)
 
 #define set_pte_atomic(ptep, pte) native_set_pte_atomic(ptep, pte)
 
